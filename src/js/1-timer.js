a62b1971edaa492985d3c3fe3e6d743a0bd8ef32
Molnia00 <bilankati@gmail.com>
2024-11-08 21:19:28 +0200

    update

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -4,3 +4,22 @@ import "flatpickr/dist/flatpickr.min.css";
 
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+
+const inputOfDATE = document.querySelector('#datetime-picker');
+const timerContainer = document.querySelector('.timer');
+const fieldContainer = document.querySelector('.field');
+
+let userSelectedDate = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+
+flatpickr("#datetime-picker", options);
