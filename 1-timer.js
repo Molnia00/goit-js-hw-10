bf377be49d03885c8bd9588c841cc9977a7311a2
Molnia00 <159462069+Molnia00@users.noreply.github.com>
2024-11-11 19:59:42 +0000

    Deploying to gh-pages from @ Molnia00/goit-js-hw-10@4bdea1b1270ab564524ba47eefc85cfa569c0e73 🚀

---
+++ 1-timer.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{f as t}from"./assets/vendor-DOPLpFvu.js";document.querySelector("#datetime-picker");document.querySelector(".timer");document.querySelector(".field");document.querySelector("[data-start]");const o={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0]),e[0]<new Date&&alert("Please choose a date in the future")}};t("#datetime-picker",o);
+//# sourceMappingURL=1-timer.js.map
